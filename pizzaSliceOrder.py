3a14d0e9431f34033843b20977aee5bd62e2f11c
taz13 <tahseen137@gmail.com>
2020-02-17 21:24:22 -0500

    Added everything

--- pizzaSliceOrder.py
+++ pizzaSliceOrder.py
@@ -1,17 +1,31 @@
 # Open a file
-fo = open("b_small.in", "r");
-print(fo.name);
-fileLines=fo.readlines()
-maxSlice = fileLines[0].split(" ")[0]
+inputFile = open("b_small.in", "r");
+print(inputFile.name);
+fileLines = inputFile.readlines()
+maxSlice = int(fileLines[0].split(" ")[0])
 numberOfPizzaType = int(fileLines[0].split(" ")[1])
 print("Maximum slice: ", maxSlice)
 print("Number of Pizza Type: ", numberOfPizzaType)
-
-
 
 pizzaSliceMap = fileLines[1].split(" ")
 print("Pizza slice map: ", pizzaSliceMap)
 print("Pizza slice map size: ", len(pizzaSliceMap))
 
-maxPizzaSliceOrder = pizzaSliceMap[numberOfPizzaType-1]
-print("Order count: ", maxPizzaSliceOrder)
+countPizzaSliceOrder = int(pizzaSliceMap[numberOfPizzaType - 1])
+
+index = numberOfPizzaType - 2;
+remainingSlices = int(maxSlice)
+
+
+def findNextIndex(endIndex):
+    while ()
+
+
+while countPizzaSliceOrder < maxSlice:
+    index = findNextIndex(index);
+    countPizzaSliceOrder = countPizzaSliceOrder + pizzaSliceMap[index];
+    print("Order count: ", findNextIndex(4))
+    if index <= 0:
+        break
+    
+print("Order count: ", countPizzaSliceOrder)
